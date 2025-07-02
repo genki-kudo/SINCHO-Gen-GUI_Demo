4d9935c0a48a6989f7054446e569f67883963e6d
Genki Kudo <gxuanji@gmail.com>
2025-07-02 19:28:00 +0900

    [fix] bag

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 
 def main():
     script = os.path.join(os.path.dirname(__file__), "gui_controller.py")
-    subprocess.run(["streamlit", "run", script, "--server.address", "0.0.0.0"]])
+    subprocess.run(["streamlit", "run", script, "--server.address", "0.0.0.0"])
 
 if __name__ == "__main__":
     main()
